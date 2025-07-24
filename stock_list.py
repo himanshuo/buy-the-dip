2cdfc092dc87e18a22286e72d118bfc30b47b824
Himanshu Ojha <himanshuo@gmail.com>
2025-07-23 19:04:23 -0700

    remove SCHG, GOOG, META and replace with ESPO, EA, AUR

--- stock_list.py
+++ stock_list.py
@@ -1,5 +1,5 @@
 SP100 = [
-    'NVDA', 'MSFT', 'AAPL', 'AMZN', 'GOOG', 'META', 'AVGO', 'TSLA', 'BRK-B', 'JPM', 'WMT', 'LLY', 'ORCL',
+    'NVDA', 'MSFT', 'AAPL', 'AMZN', 'EA', 'AUR', 'AVGO', 'TSLA', 'BRK-B', 'JPM', 'WMT', 'LLY', 'ORCL',
     'V', 'NFLX', 'MA',    'XOM', 'COST', 'JNJ', 'PG', 'PLTR', 'HD',    'BAC', 'ABBV', 'KO', 'GE', 'PM', 'CSCO', 'IBM',
     'CVX', 'WFC', 'TMUS', 'UNH', 'AMD', 'CRM', 'MS', 'DIS', 'GS', 'AXP',    'ABT', 'MCD', 'INTU', 'BX', 'RTX', 'MRK',
     'NOW', 'TXN', 'PEP', 'CAT', 'T', 'UBER', 'ISRG', 'BKNG', 'SCHW', 'BA', 'VZ', 'C', 'BLK', 'QCOM', 'SPGI', 'AMGN',
@@ -9,7 +9,7 @@ SP100 = [
 TOP_ETFS = [
 'QTUM','VOO','SPY','IVV','VTI','QQQ','VUG','VEA','IEFA','VTV','BND','AGG','IWF','GLD','IEMG','VGT','VXUS','IJH','VWO',
     'VIG','IBIT','VO','XLK','IJR','SPLG','RSP','ITOT','SCHD','BNDX','IWM','EFA','VB','IWD','VYM','IVW','SCHX','QQQM',
-    'VCIT','QUAL','SGOV','XLF','VT','SCHF','IAU','TLT','VEU','IXUS','SCHG','VV','IWR','BIL','IWB','JEPI','IVE','MBB',
+    'VCIT','QUAL','SGOV','XLF','VT','SCHF','IAU','TLT','VEU','IXUS','ESPO','VV','IWR','BIL','IWB','JEPI','IVE','MBB',
     'SPYG','DIA','MUB','BSV','VTEB','DFAC','SCHB','IEF','VCSH','VNQ','XLV','IUSB','DGRO','JPST','VGIT','VBR','VONG',
     'JEPQ','MGK','LQD','SMH','XLE','GOVT','TQQQ','SPYV','SPDW','VGK','EFV','USHY','FBTC','SHY','IUSG','XLC','USMV',
     'BIV','VXF','MDY','JAAA','VGSH','XLI','ACWI','XLY','IYW','GBTC','IGSB','IUSV',]
